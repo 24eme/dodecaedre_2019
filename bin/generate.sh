6a636ecfda084a1a90a10999d3c647f939cd0a4e
Tangui Morlier <tangui@tangui.eu.org>
2019-01-16 15:14:29 +0100

    Supprime tous les caractères de debug

--- bin/generate.sh
+++ bin/generate.sh
@@ -17,6 +17,6 @@ rm -f generated/*
 
 inkscape tmp/penta-cube-2019_laser.svg --export-pdf=generated/penta-cube-2019_decoupe.pdf --without-gui=true
 
-$CMD_CSV | sed 's/^*//' | awk -F ';' '{if ($2 && ! $5) print "sed \"s/HHHHHHHHHH/"$1"/\" tmp/penta-cube-2019_impression.svg | sed \"s/BBBBBBBBBB/"$2"/\" >  tmp/penta-cube-2019.svg ; inkscape tmp/penta-cube-2019.svg --export-pdf=generated/penta-cube-2019_"NR".pdf --without-gui=true"}'| sh
+$CMD_CSV | sed 's/^*//' | awk -F ';' '{if ($2 && ! $5) print "sed \"s/HHHHHHHHHH*/"$1"/\" tmp/penta-cube-2019_impression.svg | sed \"s/BBBBBBBBBB*/"$2"/\" >  tmp/penta-cube-2019.svg ; inkscape tmp/penta-cube-2019.svg --export-pdf=generated/penta-cube-2019_"NR".pdf --without-gui=true"}'| sh
 
 pdftk $(ls -rt generated/*) cat output penta-cube-2019.pdf
